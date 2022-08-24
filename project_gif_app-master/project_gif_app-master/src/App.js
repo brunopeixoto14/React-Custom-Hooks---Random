5d95e6e2b4175fc8573fe11c9a06c70661c6dad8
Bruno Peixoto <103580352+brunopeixoto14@users.noreply.github.com>
2022-08-24 14:35:21 -0300

    Add files via upload

---
+++ project_gif_app-master/project_gif_app-master/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import axios from 'axios';
+
+import Random from './components/Random_V1';
+import Tag from './components/Tag_V1';
+
+import './App.css';
+
+const App = () => (
+    <div>
+        <h1>Random GIF Application</h1>
+        <div className="main-container">
+            <Random />
+            <Tag />
+        </div>
+    </div>
+);
+
+export default App;
